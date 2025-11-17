57633f6aac4d14177c45e0a84be7952dac74d976
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2025-11-17 00:33:53 -0300

    Refatoração da autorização

--- controllers/note.php
+++ controllers/note.php
@@ -1,23 +1,18 @@
 <?php
 
-$config = require 'config.php';
+$config = require ('config.php');
 $db = new Database($config['database']);
 
 
 $heading = "Note";
+$currentUserId  = 1;
 
 $note = $db->query('SELECT * FROM notes where id = :id',
     [
         'id' => $_GET['id']
-    ])->fetch();
+    ])->findOrFail();
 
-if (! $note) {
-    abort();
-}
+authorize($note['user_id'] === $currentUserId);
 
-$currentUserId = 1;
 
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
 require "views/note.view.php";
