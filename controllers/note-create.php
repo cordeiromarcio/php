40a001aed233f94c826771122247facac83f26b3
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2025-11-18 02:27:45 -0300

    Atualizado com validação de campo em branco e limite de caracter

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,8 +1,30 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config['database']);
+
 $heading = "Criar Nota";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    dd($_POST);
+
+    $errors = [];
+//validação campo em branco BD
+    if (strlen($_POST["body"]) === 0) {
+        $errors["body"] = "Por favor informe o texto.";
+    }
+
+    //validação quntidade de caracteres
+    if (strlen($_POST["body"]) > 1000) {
+        $errors["body"] = "O corpo não pode ter mais de 1000 caracteres.";
+    }
+
+    if (empty($errors)) {
+
+        $db->query('insert into notes(body,user_id) values (:body,:user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+
 }
 require 'views/note-create.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -12,7 +12,7 @@
                 <a href="/notes" class="text-blue-500 underline">Voltar</a>
             </p>
 
-            <p> <?= $note['body'] ?> </p>
+            <p> <?= htmlspecialchars($note['body']) ?> </p>
 
         </div>
     </main>
